5872cd7cfe7bac062867e3ec0842882b86d94c6c
purofle <purofle@gmail.com>
2024-11-30 10:17:34 +0800

    style: reformat code

--- src/command/create_vm.rs
+++ src/command/create_vm.rs
@@ -1,16 +1,28 @@
 use anyhow::Result;
-use teloxide::{dispatching::DpHandlerDescription, dptree::{self, Handler}, prelude::{DependencyMap, Requester}, types::Message, Bot};
+use teloxide::{
+    dispatching::DpHandlerDescription,
+    dptree::{self, Handler},
+    prelude::{DependencyMap, Requester},
+    types::Message,
+    Bot,
+};
 
 use crate::{BotState, MyDialogue};
 
-pub fn set_create_vm_state(handler: Handler<'_, DependencyMap, Result<()>, DpHandlerDescription>) -> Handler<'_, DependencyMap, Result<()>, DpHandlerDescription> {
-    handler.branch(dptree::case![BotState::ReceiveName].endpoint(receive_name))
-    .branch(dptree::case![BotState::ReceiveCPUs].endpoint(receive_cpu))
+pub fn set_create_vm_state(
+    handler: Handler<'_, DependencyMap, Result<()>, DpHandlerDescription>,
+) -> Handler<'_, DependencyMap, Result<()>, DpHandlerDescription> {
+    handler
+        .branch(dptree::case![BotState::ReceiveName].endpoint(receive_name))
+        .branch(dptree::case![BotState::ReceiveCPUs].endpoint(receive_cpu))
 }
 
 async fn receive_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> Result<()> {
-
-    bot.send_message(msg.chat.id, "How many CPU cores would you like to allocate?").await?;
+    bot.send_message(
+        msg.chat.id,
+        "How many CPU cores would you like to allocate?",
+    )
+    .await?;
 
     dialogue.update(BotState::ReceiveCPUs).await?;
 
@@ -18,8 +30,11 @@ async fn receive_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> Result<()
 }
 
 async fn receive_cpu(bot: Bot, dialogue: MyDialogue, msg: Message) -> Result<()> {
-
-    bot.send_message(msg.chat.id, "How much Disk space would you like to allocate?").await?;
+    bot.send_message(
+        msg.chat.id,
+        "How much Disk space would you like to allocate?",
+    )
+    .await?;
 
     dialogue.update(BotState::ReceiveDiskSize).await?;
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 pub mod command;
 
-use command::create_vm::{set_create_vm_state, CreateVMState};
+use anyhow::Result;
+use command::create_vm::set_create_vm_state;
 use dptree::case;
 use log::info;
-use anyhow::Result;
-use teloxide::{dispatching::{dialogue::{self, InMemStorage}, UpdateHandler}, prelude::*, utils::command::BotCommands};
+use teloxide::{
+    dispatching::{
+        dialogue::{self, InMemStorage},
+        UpdateHandler,
+    },
+    prelude::*,
+    utils::command::BotCommands,
+};
 extern crate pretty_env_logger;
 
 type MyDialogue = Dialogue<BotState, InMemStorage<BotState>>;
@@ -19,7 +26,7 @@ enum SimpleCommand {
 pub enum BotState {
     #[default]
     DefaultState,
-    
+
     ReceiveName,
     ReceiveMemory,
     ReceiveCPUs,
@@ -47,50 +54,53 @@ async fn main() {
     let bot = Bot::from_env();
 
     Dispatcher::builder(bot, schema())
-    .dependencies(dptree::deps![InMemStorage::<BotState>::new()])
-    .build()
-    .dispatch()
-    .await;
+        .dependencies(dptree::deps![InMemStorage::<BotState>::new()])
+        .build()
+        .dispatch()
+        .await;
 }
 
 fn schema() -> UpdateHandler<anyhow::Error> {
     let command_handler = teloxide::filter_command::<Command, _>()
         .branch(
             case![BotState::DefaultState]
-            .branch(case![Command::Help].endpoint(help))
-            .branch(case![Command::Start].endpoint(start))
-                .branch(case![Command::Create].endpoint(create))
+                .branch(case![Command::Help].endpoint(help))
+                .branch(case![Command::Start].endpoint(start))
+                .branch(case![Command::Create].endpoint(create)),
         )
         .branch(case![Command::Cancel].endpoint(cancel));
 
-    let message_handler = Update::filter_message()
-        .branch(command_handler);
+    let message_handler = Update::filter_message().branch(command_handler);
 
     let handler = set_create_vm_state(message_handler);
 
-        dialogue::enter::<Update, InMemStorage<BotState>, BotState, _>()
-        .branch(handler)
+    dialogue::enter::<Update, InMemStorage<BotState>, BotState, _>().branch(handler)
 }
 
 async fn start(bot: Bot, _: MyDialogue, msg: Message) -> Result<()> {
-    bot.send_message(msg.chat.id, "Welcome to use Sundownfruit Cloud!").await?;
+    bot.send_message(msg.chat.id, "Welcome to use Sundownfruit Cloud!")
+        .await?;
     Ok(())
 }
 
 async fn help(bot: Bot, msg: Message) -> Result<()> {
-    bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+    bot.send_message(msg.chat.id, Command::descriptions().to_string())
+        .await?;
     Ok(())
 }
 
 async fn create(bot: Bot, dialogue: MyDialogue, msg: Message) -> Result<()> {
-    bot.send_message(msg.chat.id, "Initiating VM creation...").await?;
-    bot.send_message(msg.chat.id, "What would you like to name your VM?").await?;
+    bot.send_message(msg.chat.id, "Initiating VM creation...")
+        .await?;
+    bot.send_message(msg.chat.id, "What would you like to name your VM?")
+        .await?;
     dialogue.update(BotState::ReceiveCPUs).await?;
     Ok(())
 }
 
 async fn cancel(bot: Bot, dialogue: MyDialogue, msg: Message) -> Result<()> {
-    bot.send_message(msg.chat.id, "Cancelling the dialogue.").await?;
+    bot.send_message(msg.chat.id, "Cancelling the dialogue.")
+        .await?;
     dialogue.exit().await?;
     Ok(())
 }
